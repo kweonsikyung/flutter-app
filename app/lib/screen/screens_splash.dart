faace420b3774243e92a851d02c194cf6e4535bf
kweonsikyung <ekrk6632@naver.com>
2024-05-11 06:44:38 +0900

    fix: dafult router after splash

--- app/lib/screen/screens_splash.dart
+++ app/lib/screen/screens_splash.dart
@@ -8,7 +8,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   void moveScreen() {
-    Navigator.pushReplacementNamed(context, '/main');
+    Navigator.pushReplacementNamed(context, '/index');
   }
 
   @override
