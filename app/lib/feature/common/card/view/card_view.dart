b1663ca1bf6b9701724fcf15b806fd2d4ff3459d
kweonsikyung <ekrk6632@naver.com>
2024-05-11 07:26:53 +0900

    fix: delete bottom padding in multiple widgets

--- app/lib/feature/common/card/view/card_view.dart
+++ app/lib/feature/common/card/view/card_view.dart
@@ -118,7 +118,7 @@ class _MultipleCardWidgetsState extends State<MultipleCardWidgets> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
+      padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
--- app/lib/feature/common/list/view/list_view.dart
+++ app/lib/feature/common/list/view/list_view.dart
@@ -96,7 +96,7 @@ class MultipleListWidgets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
+      padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
