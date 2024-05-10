d028b1c94d310b88822b44ae404608599e425127
kweonsikyung <ekrk6632@naver.com>
2024-05-11 07:16:00 +0900

    add: dummy data

---
+++ app/lib/src/dummuy_data.dart
@@ -0,0 +1,100 @@
+List<String> TagData = ['animal', 'family', 'friend'];
+
+List<Map<String, String>> dataList = [
+  {
+    'type': 'animal',
+    'title': '토끼일까?',
+    'description': '크림빵 외 10명',
+    'imagePath': 'assets/animal1.jpeg',
+  },
+  {
+    'type': 'animal',
+    'title': '딩동 거미',
+    'description': '신성희 외 8명',
+    'imagePath': 'assets/animal2.jpeg',
+  },
+  {
+    'type': 'animal',
+    'title': '당근 유치원',
+    'description': '안녕달 외 12명',
+    'imagePath': 'assets/animal3.jpeg',
+  },
+  {
+    'type': 'family',
+    'title': '테푸 할아버지의 요술테이프',
+    'description': '김희수 외 12명',
+    'imagePath': 'assets/family1.jpeg',
+  },
+  {
+    'type': 'family',
+    'title': '아빠는 내가 지켜줄게',
+    'description': '고경순 외 1명',
+    'imagePath': 'assets/family2.jpeg',
+  },
+  {
+    'type': 'family',
+    'title': '위대한 가족',
+    'description': '윤진현 외 19명',
+    'imagePath': 'assets/family3.jpeg',
+  },
+  {
+    'type': 'friend',
+    'title': '친구가 미운날',
+    'description': '가사이 마라 외 8명',
+    'imagePath': 'assets/friend1.jpeg',
+  },
+  {
+    'type': 'friend',
+    'title': '진짜 일 학년 맞수가 나타났다!',
+    'description': '신순재 외 19명',
+    'imagePath': 'assets/friend2.jpeg',
+  },
+  {
+    'type': 'friend',
+    'title': '화가 나서 그랬어!',
+    'description': '레비지 제이슨 외 1명',
+    'imagePath': 'assets/friend3.jpeg',
+  },
+];
+
+List<Map<String, String>> adList = [
+  {
+    'imagePath': 'assets/ad1.png',
+  },
+  {
+    'imagePath': 'assets/ad2.png',
+  },
+];
+
+List<Map<String, String>> EventList = [
+  {
+    'imagePath': '/assets/image1.png',
+  },
+  {
+    'imagePath': '/assets/image2.png',
+  },
+  {
+    'imagePath': '/assets/image3.png',
+  },
+];
+
+List<Map<String, String>> musicList = [
+  {
+    'title': '컬러몬스터: 감정의 색깔',
+    'description': '안나 예나스 외 8명',
+    'imagePath': 'assets/audio1.jpeg',
+    'caption': '22min'
+  },
+  {
+    'title': '나는 자라요',
+    'description': '김희정 외 8명',
+    'imagePath': 'assets/audio2.jpeg',
+    'caption': '12min'
+  },
+  {
+    'title': '난 토마토 절대 안 먹어',
+    'description': '로렌 차일드 외 18명',
+    'imagePath': 'assets/audio3.jpeg',
+    'caption': '3min'
+  },
+];
